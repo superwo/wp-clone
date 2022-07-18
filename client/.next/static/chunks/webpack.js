9aadd58d27851baa614939031998cc81bc25dbbd
Radu <radu.avadanii@bludelego.it>
2022-07-18 10:22:56 +0300

    changes

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d40c548081d653e8"; }
+/******/ 		__webpack_require__.h = function() { return "bc2658eb24ac72d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
